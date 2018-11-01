4f0ff83a71a2692b9a8d33a9f758cb009609ecbd
Antonio Valdez <joanvaro90@gmail.com>
2018-10-31 19:24:11 -0600

    Filling the white and black pieces in the board

--- HorseRoute/main.rb
+++ HorseRoute/main.rb
@@ -6,8 +6,6 @@ $BlackPositions = Array.new
 $Board = Array.new(8) { Array.new(8) }
 
 def readFile(filename)
-  coordinates = Array.new 
-	
   data = File.readlines(filename)
   data.collect! { |x| x.delete("\n") }
 	
@@ -21,6 +19,21 @@ def isInsideBoard(x,y,n)
   return (x >= 1 and x <= n and y >= 1 and y <= n) ? true : false
 end
 
+def fillBoard()
+  # Fill white pieces
+  count = $WhitePositions.size() / 2
+  count.times do |val|
+    $Board[$WhitePositions[2*val+1].to_i - 1][$WhitePositions[2*val].to_i - 1] = "W"
+  end
+
+  # Fill black pieces
+  count = $BlackPositions.size() / 2
+  count.times do |val|
+    $Board[$BlackPositions[2*val+1].to_i - 1][$BlackPositions[2*val].to_i - 1] = "B"
+  end
+  
+end
+
 def moveHorse()
   dx = [-2,-1,1,2,-2,-1,1,2]
   dy = [-1,-2,-2,-1,1,2,2,1]
@@ -32,11 +45,13 @@ def moveHorse()
   $Board[movements[1].to_i - 1][movements[0].to_i - 1] = "X"
 
   dx.size().times do |mov|
-    tmpX = movements[0] + dx[mov]
-    tmpY = movements[1] + dy[mov]
+    tmpX = movements[0] + dx[mov] - 1
+    tmpY = movements[1] + dy[mov] - 1
 
-    $Board[tmpY - 1][tmpX - 1] = "S"
-    #puts "x = #{tmpX}, y = #{tmpY}"
+    if ( isInsideBoard(tmpX,tmpY,8) and ($Board[tmpY][tmpX] != "B") ) then
+      $Board[tmpY][tmpX] = "S"
+      #puts "x = #{tmpX}, y = #{tmpY}"
+    end
   end
 end
 
@@ -46,6 +61,7 @@ puts "White = #{$WhitePositions}"
 puts "Black = #{$BlackPositions}"
 
 puts isInsideBoard(4,9,8)
+fillBoard()
 moveHorse()
 
 $Board.each { |x| puts "#{x}" }
