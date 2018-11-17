bd9d6831a21b8715444ee3db75fd41eb5fc30d1f
Antonio Valdez <joanvaro90@gmail.com>
2018-11-16 20:44:09 -0600

    Changing the format of the horse position

--- HorseRoute/main.rb
+++ HorseRoute/main.rb
@@ -4,7 +4,6 @@ $BoardSize = 4
 $HorsePosition = Array.new
 $WhitePositions = Array.new
 $BlackPositions = Array.new
-$Board = Array.new($BoardSize) { Array.new($BoardSize) }
 $Tree = Array.new
 
 def readFile(filename)
@@ -18,15 +17,20 @@ def readFile(filename)
 end
 
 def isInsideBoard(x,y)
-  return (x >= 0 and x < $BoardSize and y >= 0 and y < $BoardSize) ? true : false
+  return (x > 0 and x <= $BoardSize and y > 0 and y <= $BoardSize) ? true : false
 end
 
 def fillBoard()
+  # Setting intial horse position
+  $HorsePosition[0] = "#{$HorsePosition[0]}#{$HorsePosition[1]}"
+  $HorsePosition[1] = nil
+  $HorsePosition.compact!
+
   # Fill white pieces
   count = $WhitePositions.size() / 2
   count.times do |val|
 
-    $WhitePositions[2*val] = "#{$WhitePositions[2*val].to_i - 1}#{$WhitePositions[2*val+1].to_i - 1}"
+    $WhitePositions[2*val] = "#{$WhitePositions[2*val].to_i}#{$WhitePositions[2*val+1].to_i}"
     $WhitePositions[2*val+1] = nil
 
   end
@@ -36,16 +40,15 @@ def fillBoard()
   # Fill black pieces
   count = $BlackPositions.size() / 2
   count.times do |val|
-    $Board[$BlackPositions[2*val+1].to_i - 1][$BlackPositions[2*val].to_i - 1] = "B"
 
-    $BlackPositions[2*val] = "#{$BlackPositions[2*val].to_i - 1}#{$BlackPositions[2*val+1].to_i - 1}"
+    $BlackPositions[2*val] = "#{$BlackPositions[2*val].to_i}#{$BlackPositions[2*val+1].to_i}"
     $BlackPositions[2*val+1] = nil
 
   end
 
   $BlackPositions.compact!
 
-  $Tree.push(["#{$HorsePosition[0].to_i  - 1}#{$HorsePosition[1].to_i - 1}"])
+  $Tree.push(["#{$HorsePosition[0]}"])
   
 end
 
@@ -57,11 +60,9 @@ def findPositions(node = nil)
   movements.push($HorsePosition[0].to_i)
   movements.push($HorsePosition[1].to_i)
 
-  $Board[movements[1].to_i - 1][movements[0].to_i - 1] = "X"
-
   dx.size().times do |mov|
-    tmpX = movements[0] + dx[mov] - 1
-    tmpY = movements[1] + dy[mov] - 1
+    tmpX = movements[0] + dx[mov]
+    tmpY = movements[1] + dy[mov]
 
     position = "#{tmpX}#{tmpY}"
 
@@ -77,8 +78,6 @@ def findPositions(node = nil)
         tmpArray = [position]
         $Tree.push(tmpArray)
       end
-
-      $Board[tmpY][tmpX] = "S"
     end
   end
 end
@@ -91,8 +90,8 @@ def moveHorse()
 
   tmpPosition = tmpArray.pop.chars
 
-  $HorsePosition[0] = tmpPosition[0].to_i + 1
-  $HorsePosition[1] = tmpPosition[1].to_i + 1
+  $HorsePosition[0] = tmpPosition[0].to_i
+  $HorsePosition[1] = tmpPosition[1].to_i
 
   findPositions(node)
 end
