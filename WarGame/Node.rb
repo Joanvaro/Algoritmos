faa2df24a2ae8ad43619bc5d3f85e32767ed690b
Antonio Valdez <joanvaro90@gmail.com>
2018-12-04 12:32:05 -0600

    First draft of the algorithm, change the way of store data and the searching is omitted

--- WarGame/Node.rb
+++ WarGame/Node.rb
@@ -14,10 +14,32 @@ class Node
 
     if relationship.eql?("friend") then
       @adjacent_nodes_friends << adjacent_node
+
+      @adjacent_nodes_friends.each do |friend|
+        unless friend.adjacent_nodes_friends.include?(adjacent_node) then
+          friend.adjacent_nodes_friends << adjacent_node 
+        end
+
+        friend.adjacent_nodes_enemies.each do |newFriend|
+          @adjacent_nodes_friends << newFriend unless @adjacent_nodes_friends.include?(newFriend)
+        end
+      end
+
     else
       @adjacent_nodes_enemies << adjacent_node
-    end
 
+      @adjacent_nodes_enemies.each do |enemy|
+        unless enemy.adjacent_nodes_enemies.include?(adjacent_node) then
+          enemy.adjacent_nodes_enemies << adjacent_node
+        end
+
+        enemy.adjacent_nodes_friends.each do |newEnemy|
+          @adjacent_nodes_enemies << newEnemy unless @adjacent_nodes_enemies.include?(newEnemy)
+        end
+        
+      end
+
+    end
   end
 
 end
--- WarGame/warGame.rb
+++ WarGame/warGame.rb
@@ -3,6 +3,7 @@
 require './Graph.rb'
 
 $Operations = Array.new
+$WarGraph = Graph.new()
 
 def readFile(filename)
   data = File.readlines(filename)
@@ -46,53 +47,54 @@ def BFS(root_node, search_value, relationship)
 end
 
 def setFriends(x,y)
-  #TODO Checar el error de checkeo
-  if BFS(x, y.value, "enemy") then
-    puts "-1"
+  if x.adjacent_nodes_enemies.include?(y) then
+    return -1
   else
     x.adjacent_nodes_friends.each do |friend|
-      puts "-1" if BFS(friend, y.value, "enemy")
+      return -1 if friend.adjacent_nodes_enemies.include?(y)
     end
 
-    y.adjacent_nodes_friends.each do |friend|
-      puts "-1" if BFS(friend, x.value, "enemy")
+    x.adjacent_nodes_enemies.each do |enemy|
+      return -1 if enemy.adjacent_nodes_friends.include?(y)
     end
-
     # Adding nodes to the graph if exist do not raise an error
-    $FriendGraph.add_node(x)
-    $FriendGraph.add_node(y)
+    $WarGraph.add_node(x)
+    $WarGraph.add_node(y)
 
-    $FriendGraph.add_edge(x,y, "friend")
+    $WarGraph.add_edge(x,y, "friend")
   end
+
+  return nil
 end
 
 def setEnemies(x,y)
-  ret = BFS(x, y.value, "friend")
-
-  x.adjacent_nodes_enemies.each do |enemy|
-    puts "-1" if BFS(enemy, y.value, "friend")
-  end
-
-  if ret then
-    puts "-1"
+  if x.adjacent_nodes_enemies.include?(y) then
+    return -1
   else
-    # Adding nodes to the graph if exist do not raise an error
-    $FriendGraph.add_node(x)
-    $FriendGraph.add_node(y)
+    x.adjacent_nodes_friends.each do |friend|
+      return -1 if friend.adjacent_nodes_enemies.include?(y)
+    end
 
-    $FriendGraph.add_edge(x,y, "enemy")
+    x.adjacent_nodes_enemies.each do |enemy|
+      return -1 if enemy.adjacent_nodes_friends.include?(y)
+    end
+    # Adding nodes to the graph if exist do not raise an error
+    $WarGraph.add_node(x)
+    $WarGraph.add_node(y)
+
+    $WarGraph.add_edge(x,y, "enemy")
   end
+
+  return nil
 end
 
 def areFriends(x,y)
-  return BFS(x,y.value, "friend") ? 1 : 0
+  return (x.adjacent_nodes_friends.include?(y) or y.adjacent_nodes_friends.include?(x)) ? 1 : 0
 end
 
 def areEnemies(x,y)
-  return BFS(x,y.value, "enemy") ? 1 : 0
+  return (x.adjacent_nodes_enemies.include?(y) or y.adjacent_nodes_enemies.include?(x)) ? 1 : 0
 end
-
-$FriendGraph = Graph.new()
 
 readFile(ARGV[0])
 $Nodes = Array.new($NumberOfPeople.to_i)
@@ -109,34 +111,15 @@ $Operations.each do |set|
 
   case operation
   when 1
-    setFriends($Nodes[nodeA], $Nodes[nodeB])
+    ret = setFriends($Nodes[nodeA], $Nodes[nodeB])
   when 2
-    setEnemies($Nodes[nodeA], $Nodes[nodeB])
+    ret = setEnemies($Nodes[nodeA], $Nodes[nodeB])
   when 3
-    puts areFriends($Nodes[nodeA], $Nodes[nodeB])
+    ret = areFriends($Nodes[nodeA], $Nodes[nodeB])
   when 4
-    puts areEnemies($Nodes[nodeA], $Nodes[nodeB])
+    ret = areEnemies($Nodes[nodeA], $Nodes[nodeB])
   end
+
+  puts ret unless ret.eql?(nil)
+
 end
-
-=begin
-node0 = Node.new(0)
-node1 = Node.new(1)
-node2 = Node.new(2)
-node3 = Node.new(3)
-node4 = Node.new(4)
-node5 = Node.new(5)
-
-setFriends(node0, node1)
-setFriends(node0, node3)
-setFriends(node1, node4)
-
-setEnemies(node2, node5)
-
-puts areFriends(node0, node4)
-puts areFriends(node0, node5)
-
-puts areEnemies(node2, node5)
-
-setEnemies(node0, node4)
-=end
